91b1150230f209e5e1a54fbd4a8949af3a853f26
Ivan Santiago <ivansantiago.junior@gmail.com>
2025-02-03 13:20:36 -0300

    test: adapt tests to use cyclopts

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,54 +1,69 @@
 import locale
 
 import pytest
-from typer.testing import CliRunner
+from rich.text import Text
 
+from resma import __version__
 from resma.main import app
 
-runner = CliRunner()
 
-
-def test_start(temp_dir, monkeypatch):
+def test_start(temp_dir, monkeypatch, capsys: pytest.CaptureFixture):
     monkeypatch.chdir(temp_dir)
-    result = runner.invoke(app, ['start', 'project'])
+    app('start project')
 
-    assert result.exit_code == 0
+    output = Text.from_ansi(capsys.readouterr().out).plain
 
-
-def test_start_project_already_exists(temp_dir, monkeypatch):
-    monkeypatch.chdir(temp_dir)
-
-    runner.invoke(app, ['start', 'project'])
-    result = runner.invoke(app, ['start', 'project'])
-
-    assert result.exit_code == 1
-    assert 'File already exists' in result.output
+    assert 'Project project created successfully' == output
 
 
-def test_build_resma_project_not_found(temp_dir, monkeypatch):
+def test_start_project_already_exists(
+    temp_dir, monkeypatch, capsys: pytest.CaptureFixture
+):
     monkeypatch.chdir(temp_dir)
 
-    result = runner.invoke(app, ['build'])
+    app('start project')
+    with pytest.raises(SystemExit) as e:
+        app('start project')
 
-    assert result.exit_code == 1
-    assert 'Not a resma project' in result.output
+    assert e.value.code == 1
+    assert 'File project already exists' in capsys.readouterr().err
 
 
-def test_build_empty_project(temp_dir, monkeypatch):
+def test_build_resma_project_not_found(
+    temp_dir, monkeypatch, capsys: pytest.CaptureFixture
+):
     monkeypatch.chdir(temp_dir)
 
-    runner.invoke(app, ['start', 'project'])
+    with pytest.raises(SystemExit) as e:
+        app('build')
+
+    assert e.value.code == 1
+    assert 'Not a resma project' in capsys.readouterr().err
+
+
+def test_build_empty_project(
+    temp_dir, monkeypatch, capsys: pytest.CaptureFixture
+):
+    monkeypatch.chdir(temp_dir)
+
+    app('start project')
 
     monkeypatch.chdir('project')
-    result = runner.invoke(app, ['build'])
+    with pytest.raises(SystemExit) as e:
+        app('build')
 
-    assert result.exit_code == 1
-    assert 'Content and Templates directories cannot be empty' in result.output
+    assert e.value.code == 1
+    assert (
+        'Content and Templates directories cannot be empty'
+        in capsys.readouterr().err
+    )
 
 
-def test_build_resma_table_not_found_in_config_toml(temp_dir, monkeypatch):
+def test_build_resma_table_not_found_in_config_toml(
+    temp_dir, monkeypatch, capsys: pytest.CaptureFixture
+):
     monkeypatch.chdir(temp_dir)
-    runner.invoke(app, ['start', 'project'])
+    app('start project')
 
     monkeypatch.chdir('project')
 
@@ -57,35 +72,36 @@ def test_build_resma_table_not_found_in_config_toml(temp_dir, monkeypatch):
         'config.toml', 'w', encoding=locale.getpreferredencoding(False)
     ) as f:
         f.write('')
-    result = runner.invoke(app, ['build'])
 
-    assert result.exit_code == 1
-    assert 'config.toml should have a resma table' in result.output
+    with pytest.raises(SystemExit) as e:
+        app('build')
+
+    assert e.value.code == 1
+    assert 'config.toml should have a resma table' in capsys.readouterr().err
 
 
 @pytest.mark.parametrize(
-    ('args', 'expected_output', 'expected_exit_code'),
+    ('args', 'expected_output'),
     [
-        ([], 'resma --help', 0),
+        ([], 'resma --help'),
         (
             ['--help'],
-            'Resma CLI Static Site Generator',
-            0,
+            'Usage',
         ),
-        (['--version'], '0.1.0', 0),
+        (['--version'], __version__),
     ],
     ids=['default', 'help', 'version'],
 )
-def test_main(args, expected_output, expected_exit_code):
-    result = runner.invoke(app, args)
+def test_main(args, expected_output, capsys: pytest.CaptureFixture):
+    app(args)
 
-    assert result.exit_code == expected_exit_code
-    assert expected_output in result.output
+    assert expected_output in capsys.readouterr().out
 
 
-def test_build_full_resma_project_ok(full_resma_project, monkeypatch):
+def test_build_full_resma_project_ok(
+    full_resma_project, monkeypatch, capsys: pytest.CaptureFixture
+):
     monkeypatch.chdir(full_resma_project)
-    result = runner.invoke(app, ['build'])
+    app('build')
 
-    assert result.exit_code == 0
-    assert 'Site built successfully' in result.output
+    assert 'Site built successfully' in capsys.readouterr().out
